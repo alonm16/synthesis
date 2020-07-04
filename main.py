517753948cc6564b775e7a3e4cda025d5c5245be
ALON <alonm774@gmail.com>
2020-07-04 17:33:06 +0300

    added eval

--- main.py
+++ main.py
@@ -1,4 +1,35 @@
 
-if __name__ == "__main__":
-    print('hi')
+def terminal_rules(grammer):
+    return [('S', '5')]
 
+
+def grow(P, grammer, spec):
+    return []
+
+
+def prone(P, new_programs):
+    return new_programs
+
+
+def validate(p, spec):
+    for (i, o) in spec:
+        if eval(p, {"__builtins__": None}, {'i': i}) != o:
+            return False
+    return True
+
+
+def bottom_up(grammer, spec):
+    P = terminal_rules(grammer)
+    while True:
+        new_programs = grow(P, grammer, spec)
+        new_programs = prone(P, new_programs)
+        if not new_programs:
+            return 'no program'
+        P += new_programs
+        for p in P:
+            if p[0] == 'S' and validate(p, spec):  #assuming p is a tuple of (V, program)
+                return p
+
+
+if __name__ == "__main__":
+    print(validate('i+1', [(1, 2)]))
