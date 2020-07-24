85783a9a7e555115053e40602e466e2069307f79
ALON <alonm774@gmail.com>
2020-07-24 20:42:29 +0300

    started implementing grow

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
+"""if every nonterminal in the programs is already in a program we created then a new program can be created"""
+def can_create_program(non_terminals,keys):
+    for non_terminal in non_terminals:
+        if not non_terminal in keys:
+            return False
+    return True
 
-def terminal_rules(grammer):
-    return [('S', '5')]
+
+def create_programs(P, rule): #not good!
+    new_programs = []
+    for symbol in rule:
+        if symbol.isupper():
+            for i in range(len(new_programs)):
+                for old_program in P[symbol]:
+                    new_programs[i] += old_program
+        else:
+            for i in range(len(new_programs)):
+                new_programs[i] += symbol #TODO check whether its a function
+    return new_programs
 
 
-def grow(P, grammer, spec):
-    return []
 
-
-def prone(P, new_programs):
+def grow(P, derivation_rules, spec):  #Todo pruning and child form last iteration
+    keys = P.keys()
+    new_programs = {}
+    for derivation_rule in derivation_rules:
+        for single_rule in derivation_rule[1]:
+            non_terminals = [symbol for symbol in single_rule if symbol.isupper()]
+            if can_create_program(non_terminals, keys):
+                new_programs[derivation_rule[0]] += [create_programs(P, single_rule)]
     return new_programs
 
 
@@ -18,11 +38,27 @@ def validate(p, spec):
     return True
 
 
+def parse_grammer(grammer):
+    derivation_rules = {}
+    terminals_rules = {}
+    for derivation_rule in grammer:
+        left, right = derivation_rule.split('::=')
+        right = right.split()
+        if len(right) == 1 and not (right[0]).isupper():
+            if left not in terminals_rules.keys():
+                terminals_rules[left] = []
+            terminals_rules[left] += [right]
+        if left not in derivation_rules.keys():
+            derivation_rules[left] = []
+        derivation_rules[left] += [right]
+    return derivation_rules, terminals_rules
+
+
 def bottom_up(grammer, spec):
-    P = terminal_rules(grammer)
+    derivation_rules, terminals_rules = parse_grammer(grammer)
+    P = terminals_rules
     while True:
-        new_programs = grow(P, grammer, spec)
-        new_programs = prone(P, new_programs)
+        new_programs = grow(P, derivation_rules, spec)
         if not new_programs:
             return 'no program'
         P += new_programs
@@ -32,4 +68,9 @@ def bottom_up(grammer, spec):
 
 
 if __name__ == "__main__":
-    print(validate('i+1', [(1, 2)]))
+    grammer, terminals = parse_grammer(["C ::= 'a'", "C ::= 'b'", "C ::= 'c'", "S ::= C", "S ::= ( S + S )"
+            , "S ::= ( S * N )", "S ::= S [ N ]" , "S ::= x", "N ::= 0", "N ::= 1", "N ::= ( N + N )"])
+    print("grammer")
+    print(grammer)
+    print("terminals")
+    print(terminals)
