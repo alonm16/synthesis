f2345d47c1bc7335baf1ba19cc53cc0a265b5966
ALON <alonm774@gmail.com>
2020-07-25 16:44:47 +0300

    fixed grow

--- main.py
+++ main.py
@@ -19,12 +19,12 @@ def create_programs(P, rule):
             new_programs = new_programs[cur_len:]
             if cur_len == 0:
                 for old_program in P[symbol]:
-                    new_programs += [old_program]
+                    new_programs += [old_program[:]]
         else:
             for i in range(cur_len):
                 new_programs[i] += symbol #TODO check whether its a fun ction
             if cur_len == 0:
-                new_programs = [symbol]
+                new_programs = [[symbol]]
     return new_programs
 
 
@@ -32,11 +32,13 @@ def create_programs(P, rule):
 def grow(P, derivation_rules, spec):  #Todo pruning and child form last iteration
     keys = P.keys()
     new_programs = {}
-    for derivation_rule in derivation_rules:
-        for single_rule in derivation_rule[1]:
+    for var in derivation_rules:
+        for single_rule in derivation_rules[var]:
             non_terminals = [symbol for symbol in single_rule if symbol.isupper()]
             if can_create_program(non_terminals, keys):
-                new_programs[derivation_rule[0]] += [create_programs(P, single_rule)]
+                if var not in new_programs.keys():
+                    new_programs[var] = []
+                new_programs[var] += create_programs(P, single_rule)
     return new_programs
 
 
@@ -46,12 +48,14 @@ def validate(p, spec):
             return False
     return True
 
+
 """creating terminal rules and all derivation rules as a dectionary of the form: Var -> list of rules derived by the var"""
 def parse_grammer(grammer):
     derivation_rules = {}
     terminals_rules = {}
     for derivation_rule in grammer:
         left, right = derivation_rule.split('::=')
+        left = left.strip()
         right = right.split()
         if len(right) == 1 and not (right[0]).isupper():
             if left not in terminals_rules.keys():
@@ -65,21 +69,25 @@ def parse_grammer(grammer):
 
 def bottom_up(grammer, spec):
     derivation_rules, terminals_rules = parse_grammer(grammer)
-    P = terminals_rules
+    P = {}
+    for key in terminals_rules.keys():
+        P[key] = terminals_rules[key][:]
+    if 'S' not in P.keys():
+        P['S'] = []
     while True:
         new_programs = grow(P, derivation_rules, spec)
         if not new_programs:
             return 'no program'
-        P += new_programs
-        for p in P:
-            if p[0] == 'S' and validate(p[1], spec):  #assuming p is a tuple of (V, program)
+        for var in new_programs.keys():
+            if var not in P.keys():
+                P[var] = []
+            P[var] += new_programs[var]
+        for p in P['S']:
+            if validate(p, spec):
                 return p
 
 
 if __name__ == "__main__":
-    grammer, terminals = parse_grammer(["C ::= 'a'", "C ::= 'b'", "C ::= 'c'", "S ::= C", "S ::= ( S + S )"
-            , "S ::= ( S * N )", "S ::= S [ N ]" , "S ::= x", "N ::= 0", "N ::= 1", "N ::= ( N + N )"])
-    print("grammer")
-    print(grammer)
-    print("terminals")
-    print(terminals)
+    grammer = ["C ::= 'a'", "C ::= 'b'", "C ::= 'c'", "S ::= C", "S ::= ( S + S )"
+            , "S ::= ( S * N )", "S ::= S [ N ]" , "S ::= x", "N ::= 0", "N ::= 1", "N ::= ( N + N )"]
+    bottom_up(grammer, [])
